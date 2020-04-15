dff78fcb704fc2b1245fe6ad0c2da85585bce7d6
Shyam Bahety <bahetyshyam@gmail.com>
2020-04-15 23:45:27 +0530

    changed purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,13 @@
-// postcss.config.js
 const purgecss = require('@fullhuman/postcss-purgecss')({
-
-    // Specify the paths to all of the template files in your project 
     content: [
-        './src/**/*.html',
-        './src/**/*.vue',
-        './src/**/*.jsx',
-        // etc.
+      './src/**/*.jsx',
+      './src/**/*.js',
+      './public/index.html'
     ],
     css: ['./src/assets/tailwind.css'],
     // Include any special characters you're using in this regular expression
     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-})
+  })
 
 
 module.exports = {
